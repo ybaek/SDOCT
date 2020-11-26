b48a87c1eee60d40c031d8ef1178ee5b9841645c
ybaek <overhead012@gmail.com>
2020-11-26 22:56:09 +0900

    Produced ROC examples

--- validation.R
+++ validation.R
@@ -18,7 +18,7 @@ validate_roc <- function(coef_train, labels, train, test, X, Y) {
 }
 
 # Testing the function with an unregularized linear fit
-data <- readRDS("../data/fit_data.Rds")
+data <- readRDS("./data/fit_data.Rds")
 attach(data)
 N <- dim(Z_s)[1]; P <- dim(Z_s)[2]; Q <- dim(Y_s)[2]; J <- max(ids)
 set.seed(2020-11-25)
@@ -27,28 +27,17 @@ test <- setdiff(1:N, train)
 lss <- lm(Y_s[train,] ~ Z_s[train,]-1)
 # fields::image.plot(t(coef(lss)), col = hcl.colors(12, "RdBu", rev=T))
 lss_rocs <- validate_roc(coef(lss), labels, train, test, Z_s, Y_s)
-# plot(lss_roc, main = "ROC curve for least squares fit")
-# 
-# # Same model, but now standardized
-# Z_ss <- scale(Z_s, F, T)
-# Y_ss <- scale(Y_s, F, T)
-# lss_s <- lm(Y_ss[train,] ~ Z_ss[train,]-1)
+plot(lss_roc$roc, main = "ROC curve for least squares fit")
+
+# Same model but standardized
+Z_ss <- scale(Z_s, F, T)
+Y_ss <- scale(Y_s, F, T)
+lss_s <- lm(Y_ss[train,] ~ Z_ss[train,]-1)
 # fields::image.plot(t(coef(lss_s)), col = hcl.colors(12, "RdBu", rev=T))
-# predicted_s <- Z_ss[train,] %*% coef(lss_s)
-# df_for_logistic_s <- cbind.data.frame(labels = labels[train], predicted_s)
-# lss_classifier_s <- glm(labels ~ . - 1, data = df_for_logistic_s, binomial)
-# # New data first fit for test data set, then those values for logistic regression
-# pred_test_s <- as.data.frame(Z_s[test,] %*% coef(lss_s))
-# lss_probs_s <- predict(lss_classifier_s, pred_test_s, type = "response")
-# 
-# library(pROC)
-# lss_roc_s <- roc(labels[test] ~ lss_probs_s)
-# lss_pauc_s <- auc(roc(labels[test] ~ lss_probs_s,
-#                     partial.auc = c(.85, 1), partial.auc.correct = FALSE, partial.auc.focus = "sp"), 
-#                 partial.auc = c(.85, 1), partial.auc.correct = F, partial.auc.focus = "sp")
-# lss_pauc_s / .15 # Normalize
-# lines(lss_roc_s, col = 2)
-# legend("bottomright",
-#        legend = c("Raw scale (AUC .83 / pAUC .65)",
-#                   "Standardized (AUC .86 / pAUC .63)"),
-#        col = 1:2, lty = 1, lwd = 2, cex = .8)
+lss_s_rocs <- validate_roc(coef(lss_s), labels, train, test, Z_ss, Y_ss)
+lines(lss_s_rocs$roc, col = 2)
+
+legend("bottomright",
+        legend = c("Raw scale (AUC .84 / pAUC .65)",
+                   "Standardized (AUC .86 / pAUC .63)"),
+        col = 1:2, lty = 1, lwd = 2, cex = .8)
