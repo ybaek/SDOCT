8a7af57e9aab1ea9eacfe90ebabf9092c3a79522
ybaek <overhead012@gmail.com>
2020-12-01 20:08:36 +0900

    Added train-test partition

--- main_lmm.R
+++ main_lmm.R
@@ -1,14 +1,20 @@
 # This package ns must be attached
 library(GIGrvg)
+source("./adj_grid.R")
 source("./utilities.R")
-
 input <- readRDS("./data/fit_data.Rds")
-data <- input[c("ids", "mis_inds", "small_inds", "zmeans", "ymeans", "labels")]
-data$X <- input$Z_s
-data$Y <- input$Y_s
+A <- adj_grid(8)
+
+# Pre-processing training datasets
+data <- input[c("small_inds", "zmeans", "ymeans", "labels")]
+data$X <- input$Z_s[input$train,]
+data$Y <- input$Y_s[input$train,]
+data$ids <- input$ids[input$train]
+data$mis_inds <- which(is.na(data$Y), arr.ind = TRUE)
+data$mis_adjs <- apply(A[unique(data$mis_inds)[,2],], 1, function(x) which(!!x))
 data$Y[data$mis_inds] <- rnorm(dim(data$mis_inds)[1]) * 3 # Initialize the missing values
-data$n_ns <- sapply(input$mis_adjs, length) # no. of neighbors for each missing location
-data$neighbors <- unlist(input$mis_adjs) # all neighbors of missing locations ordered by patients
+data$n_ns <- sapply(data$mis_adjs, length) # no. of neighbors for each missing location
+data$neighbors <- unlist(data$mis_adjs) # all neighbors of missing locations ordered by patients
 hyper <- list(rho = .99, beta_var0 = 100, c0 = 1/3)
 l <- 15 / 768 * (2*pi) 
 hyper$prec_y <- hyper$rho * input$Lap + diag(1-hyper$rho, ncol(data$Y))
@@ -19,7 +25,7 @@ inits1 <- list( beta = c(coef(lm(data$Y ~ data$X - 1))),
                sig2inv = 1, tau2inv = 1, c2 = rep(.25^2, length(data$small_inds)),
                gamma = array(0, dim = c(ncol(data$X), ncol(data$Y), max(data$ids))),
                theta = matrix(0, nrow(data$Y), ncol(data$Y)) )
-inits1$theta <- (data$Y - data$X %*% matrix(inits$beta, ncol(data$X))) * matrix(rnorm(nrow(data$Y)*ncol(data$Y)), nrow(data$Y))
+inits1$theta <- (data$Y - data$X %*% matrix(inits1$beta, ncol(data$X))) * matrix(rnorm(nrow(data$Y)*ncol(data$Y)), nrow(data$Y))
 
 inits2 <- list( beta = c(coef(lm(data$Y ~ data$X - 1))),
                sig2inv = 1, c2 = rep(.25^2, length(data$small_inds)),
@@ -31,15 +37,15 @@ mcmc <- list(I = 1800, burnin = 300)
 
 # Compile the Gibbs sampler routine
 library(Rcpp)
-sourceCpp("samplers_lmm.cpp")
-sourceCpp("samplers_mv.cpp")
+sourceCpp("samplers_lmm.cpp", verbose = T, rebuild = T)
+sourceCpp("samplers_mv.cpp", verbose = T, rebuild = T)
 chain1 <- mainSampler_lmm(data, inits1, hyper, mcmc)
-chain2 <- mainSampler_lmm(data, inits2, hyper, mcmc)
+chain2 <- mainSampler_mv(data, inits2, hyper, mcmc)
 # See if standardization leads to drastically different results
-data$X <- scale(input$Z_s, F, T)
-data$Y <- scale(input$Y_s, F, T)
+data$X <- scale(data$X, F, T)
+data$Y <- scale(data$Y, F, T)
 chain3 <- mainSampler_lmm(data, inits1, hyper, mcmc)
-chain4 <- mainSampler_lmm(data, inits2, hyper, mcmc)
+chain4 <- mainSampler_mv(data, inits2, hyper, mcmc)
 saveRDS(chain1, "./objects/samples_lmm1_cpp.Rds")
 saveRDS(chain2, "./objects/samples_mv1_cpp.Rds")
 saveRDS(chain3, "./objects/samples_lmm2_cpp.Rds")
--- preprocessing.R
+++ preprocessing.R
@@ -3,33 +3,39 @@ df <- readRDS("./data/macula_cross14-17.Rds")
 pseudomean_Y <- readRDS("./data/control_m.Rds")
 pseudomean_Z <- readRDS("./data/control_cp.Rds")
 ids <- as.integer(factor(df$patientid, levels = sort(unique(df$patientid))))
-id_list <- lapply(1:max(ids), function(x) which(ids==x))
 labels <- df$group
 Z <- sweep(as.matrix(df[,(2+1):(2+288)]), 2, pseudomean_Z, "-")
 Y <- sweep(as.matrix(df[,(2+288+1):(2+288+64)]), 2, pseudomean_Y, "-")
-mis_inds <- which(is.na(Y), arr.ind = TRUE)
+# Partition the data on PATIENT strata
+set.seed(2020-11-25)
+J <- max(ids)
+train <- sort(sample(J, J*.8))
+test <- setdiff(1:J, train)
+train_rows_bool <- ids %in% train
+test_rows_bool <- ids %in% test
 # Prior information
 source("./adj_grid.R")
 source("./prior_distances.R")
 ## aggregating by 4 for predictors
 prior_mean <- prior_mean[seq(1, 288, by = 8), ]
 distMat <- distMat[seq(1, 288, by = 8), seq(1, 288, by = 8)]
-small_inds <- which(!prior_mean)
 A <- adj_grid(8)
-mis_adjs <- apply(A[unique(mis_inds)[,2],], 1, function(x) which(!!x))
 Lap <- diag(rowSums(A)) - A
+small_inds <- which(!prior_mean)
+## Scaling and centering data
 Z_a <- Z %*% (diag(1, 36) %x% rep(.125,8))
 colnames(Z_a) <- colnames(Z)[seq(1, 288, by = 8)]
-# I've come to realize they are measured in same units, so standardization is not a good idea
 Z_s <- scale(Z_a, T, F)
 Y_s <- scale(Y, T, F)
 zmeans <- attr(Z_s, "scaled:center")
 ymeans <- attr(Y_s, "scaled:center")
+# I've come to realize they are measured in same units, so standardization is not a good idea
 # zsds <- attr(Z_s, "scaled:scale")
 # ysds <- attr(Y_s, "scaled:scale")
 
+# mis_adjs <- apply(A[unique(mis_inds)[,2],], 1, function(x) which(!!x))
+
 data <- list(Z_s = Z_s, Y_s = Y_s, zmeans = zmeans, ymeans = ymeans, 
-             small_inds = small_inds, mis_adjs = mis_adjs, mis_inds = mis_inds, 
-             distMat = distMat, Lap = Lap, 
-             ids = ids, id_list = id_list, labels = labels)
+             small_inds = small_inds, distMat = distMat, Lap = Lap, 
+             ids = ids, train = train_rows_bool, test = test_rows_bool, labels = labels)
 saveRDS(data, file = "./data/fit_data.Rds")
