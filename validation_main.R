a291acfb223ea646d7e6de2f80b1d9ee3528f2dd
ybaek <overhead012@gmail.com>
2020-12-04 20:56:48 +0900

    Minor changes

--- validation_main.R
+++ validation_main.R
@@ -3,10 +3,21 @@ source("validation.R")
 # Testing the function with an unregularized linear fit
 data <- readRDS("./data/fit_data.Rds")
 lss <- with(data, lm(Y_s[train,] ~ Z_s[train,]-1))
-# fields::image.plot(t(coef(lss)), col = hcl.colors(12, "RdBu", rev=T))
+
+png("./images/coefficient.png", width = 6, height = 6, units = "in", res = 120)
+fields::image.plot(t(coef(lss)), col = hcl.colors(12, "RdBu", rev=T), main = "Least squares estimator")
+dev.off()
+
 preds_lss <- as.data.frame(data$Z_s %*% coef(lss))
-lss_rocs <- validate_roc(preds_lss, labels, train, test, data$Z_s, data$Y_s)
+lss_rocs <- validate_roc(preds_lss, data$labels, data$train, data$test, data$Z_s, data$Y_s)
+
+png("./images/roc.png", width = 6, height = 6, units = "in", res = 120)
 plot(lss_rocs$roc, main = "ROC curve for least squares fit")
+legend("bottomright",
+       legend = c(paste0("AUC=", round(lss_rocs$roc$auc, 2)*100, "%"), 
+                    paste0("pAUC=", round(lss_rocs$pauc, 2)*100, "%")),
+       bty = "n")
+dev.off()
 
 # Same model but standardized makes no difference
 # Z_ss <- scale(data$Z_s, F, T)
@@ -19,6 +30,29 @@ plot(lss_rocs$roc, main = "ROC curve for least squares fit")
 
 bayesfit <- readRDS("./objects/samples_lmm1_cpp.Rds")
 coef_b <- matrix(rowMeans(bayesfit[1:2304,]), 36)
-preds_b <- as.data.frame(data$Z_s %*% coef_b)
-bayes_rocs <- validate_roc(preds_b, labels, train, test, data$Z_s, data$Y_s)
-lines(bayes_rocs$roc, col = 2) # You can't tell the difference
+fields::image.plot(t(coef_b), col = hcl.colors(12, "RdBu", rev=T))
+
+png("./images/c_densities.png", width = 6, height = 6, units = "in", res = 120)
+plot(density(bayesfit[(2304+1+1+1),]^.5, from = 0), 
+     col = scales::alpha(1, .2), xlim = c(0, 2), ylim = c(0, 5),
+     main = "Posterior KDEs of c")
+for (i in 2:1712) {
+    lines(density(bayesfit[(2304+1+1+i),]^.5, from = 0), col = scales::alpha(1, .1))
+}
+dev.off()
+
+# Standardization can lead to uninterpretable model fit
+bayesfit_s <- readRDS("./objects/samples_lmm2_cpp.Rds")
+png("./images/c_densities_s.png", width = 6, height = 6, units = "in", res = 120)
+plot(density(bayesfit_s[(2304+1+1+1),]^.5, from = 0), 
+     col = scales::alpha(1, .2), xlim = c(0, 3), ylim = c(0, 3),
+     main = "Prior decision incoherent with standardization")
+for (i in 2:1712) {
+    lines(density(bayesfit_s[(2304+1+1+i),]^.5, from = 0), col = scales::alpha(1, .1))
+}
+dev.off()
+
+# preds_b <- as.data.frame(data$Z_s %*% coef_b)
+# bayes_rocs <- validate_roc(preds_b, data$labels, data$train, data$test, data$Z_s, data$Y_s)
+# lines(bayes_rocs$roc, col = 2) # You can't tell the difference
+
