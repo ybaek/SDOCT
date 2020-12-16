043a35a5c46c0da9a9874b258e764ed9d8bae536
ybaek <overhead012@gmail.com>
2020-12-16 20:08:39 +0900

    Added more postprocessing script

--- validation_main.R
+++ validation_main.R
@@ -1,58 +1,112 @@
+library(ggplot2)
+library(reshape2)
+library(scales)
 source("validation.R")
+source("prior_distances.R")
 
-# Testing the function with an unregularized linear fit
-data <- readRDS("./data/fit_data.Rds")
-lss <- with(data, lm(Y_s[train,] ~ Z_s[train,]-1))
+# Downsampling prior mean
+prior_mean <- prior_mean[seq(1, 288, 8), ]
 
-png("./images/coefficient.png", width = 6, height = 6, units = "in", res = 120)
-fields::image.plot(t(coef(lss)), col = hcl.colors(12, "RdBu", rev=T), main = "Least squares estimator")
-dev.off()
+# Plotting the coefficient
+samples <- readRDS("./objects/samples_lmm_cpp.Rds")
+pm <- matrix(rowMeans(samples[1:2304, ]), 36)
+g <- ggplot() +
+    geom_tile(data = reshape2::melt(pm),
+              aes(as.numeric(Var1), as.numeric(Var2), fill = value))
+# Generating a heatmap
+g <- g + labs(x = "cpRNFL", y = "macula", fill = "Deviation") +
+    scale_fill_distiller(palette = "RdBu",
+        direction = -1, lim = c(-1, 1) * max(abs(pm))) +
+    theme(panel.background = element_blank(),
+          axis.text.x = element_blank(),
+          axis.ticks.x = element_blank(),
+          axis.text.y = element_blank(),
+          axis.ticks.y = element_blank())
+# Add bounding box to heatmap for comparison to prior
+prior_inds <- which(!!prior_mean, arr.ind = TRUE)
+prior_inds_y <- unname(prior_inds[, 2])
+prior_inds_x <- as.integer(rownames(prior_inds))
+prior_inds_x <- ifelse(prior_inds_x <= 121,
+                       prior_inds_x %/% 8 + 1,
+                       (prior_inds_x - 480) %/% 8 + 1)
+g <- g +
+    geom_rect(
+        data = NULL,
+        aes(xmin = prior_inds_x - .5,
+            xmax = prior_inds_x + .5,
+            ymin = prior_inds_y - .5,
+            ymax = prior_inds_y + .5),
+        fill = "transparent", color = "grey50"
+    )
+ggsave("./images/coefficient_bayes.png", g, "png",
+       width = 6, height = 6, units = "in", dpi = 150)
 
-preds_lss <- as.data.frame(data$Z_s %*% coef(lss))
-lss_rocs <- validate_roc(preds_lss, data$labels, data$train, data$test, data$Z_s, data$Y_s)
-
+# Plotting ROC curves
+preds_lss <- as.data.frame(data$Z_s %*% pm)
+lss_rocs <- validate_roc(preds_lss, data$labels,
+                         data$train, data$test, data$Z_s, data$Y_s)
+lss_rocs$pauc # 39%
+# Another decision rule
+# No logistic regression, just make a decision based on
+# negative deviations
+# Raw vs. smoothed (w/ cpRNFL info.)
+raw_count_roc <- pROC::roc(data$labels[data$test] ~ counts_raw)
+raw_count_proc <- pROC::roc(data$labels[data$test] ~ counts_raw,
+                            partial.auc = c(.85, 1),
+                            partial.auc.correct = F,
+                            partial.auc.focus = "sp")
+pROC::auc(raw_count_proc,
+          partial.auc = c(.85, 1),
+          partial.auc.correct = F,
+          partial.auc.focus = "sp") / .15 # 25%
+lss_count_roc <- pROC::roc(data$labels[data$test] ~ counts_smoothed)
+lss_count_proc <- pROC::roc(data$labels[data$test] ~ counts_smoothed,
+                            partial.auc = c(.85, 1),
+                            partial.auc.correct = F,
+                            partial.auc.focus = "sp")
+pROC::auc(lss_count_proc,
+          partial.auc = c(.85, 1),
+          partial.auc.correct = F,
+          partial.auc.focus = "sp") / .15 # 23%
+# Plot
 png("./images/roc.png", width = 6, height = 6, units = "in", res = 120)
-plot(lss_rocs$roc, main = "ROC curve for least squares fit")
+plot(lss_rocs$roc, main = "ROC curves", col = 2)
+lines(raw_count_roc, col = 1)
+lines(lss_count_roc, col = 4)
 legend("bottomright",
-       legend = c(paste0("AUC=", round(lss_rocs$roc$auc, 2)*100, "%"), 
-                    paste0("pAUC=", round(lss_rocs$pauc, 2)*100, "%")),
-       bty = "n")
+       legend = c("SDM (71% / 25%)",
+                  "Smoothed SDM (76% / 23%)",
+                  "Smoothed + Logistic (67% / 39%)"),
+       lty = 1, col = c(1, 4, 2))
 dev.off()
 
-# Same model but standardized makes no difference
-# Z_ss <- scale(data$Z_s, F, T)
-# Y_ss <- scale(data$Y_s, F, T)
-# lss_s <- lm(Y_ss[train,] ~ Z_ss[train,]-1)
-# preds_lss_s <- as.data.frame(data$Z_s %*% coef(lss_s))
-# # fields::image.plot(t(coef(lss_s)), col = hcl.colors(12, "RdBu", rev=T))
-# lss_s_rocs <- validate_roc(preds_lss_s, labels, train, test, Z_ss, Y_ss)
-# lines(lss_s_rocs$roc, col = 2)
-
-bayesfit <- readRDS("./objects/samples_lmm1_cpp.Rds")
-coef_b <- matrix(rowMeans(bayesfit[1:2304,]), 36)
-fields::image.plot(t(coef_b), col = hcl.colors(12, "RdBu", rev=T))
-
-png("./images/c_densities.png", width = 6, height = 6, units = "in", res = 120)
-plot(density(bayesfit[(2304+1+1+1),]^.5, from = 0), 
-     col = scales::alpha(1, .2), xlim = c(0, 2), ylim = c(0, 5),
-     main = "Posterior KDEs of c")
+# Inference: on other possible model parameters
+# Why we can diagnose ill model fit
+c_samples <- samples[(2304 + 1 + 1):(2304 + 1 + 1712), ]^.5
+c_densities <- apply(c_samples, 1, function(x) density(x, from = 0))
+plot_xmax <- max(sapply(c_densities, function(y) max(y$x)))
+plot_ymax <- max(sapply(c_densities, function(x) max(x$y)))
+png("./images/densities.png", width = 6, height = 6, units = "in", res = 120)
+plot(c_densities[[1]],
+     xlab = "c",
+     ylab = "density",
+     main = "",
+     xlim = c(0, plot_xmax),
+     ylim = c(0, plot_ymax),
+     col = scales::alpha(1, .2))
 for (i in 2:1712) {
-    lines(density(bayesfit[(2304+1+1+i),]^.5, from = 0), col = scales::alpha(1, .1))
+    lines(c_densities[[i]], col = scales::alpha(1, .2))
 }
 dev.off()
 
-# Standardization can lead to uninterpretable model fit
-bayesfit_s <- readRDS("./objects/samples_lmm2_cpp.Rds")
-png("./images/c_densities_s.png", width = 6, height = 6, units = "in", res = 120)
-plot(density(bayesfit_s[(2304+1+1+1),]^.5, from = 0), 
-     col = scales::alpha(1, .2), xlim = c(0, 3), ylim = c(0, 3),
-     main = "Prior decision incoherent with standardization")
-for (i in 2:1712) {
-    lines(density(bayesfit_s[(2304+1+1+i),]^.5, from = 0), col = scales::alpha(1, .1))
-}
-dev.off()
+psiMean <- matrix(0, 36, 36)
+psiMean[lower.tri(psiMean, diag = T)] <- rowMeans(
+    samples[(2304 + 1 + 1712 + 1):(2304 + 1 + 1712 + 666), ]
+)
+psiMean[upper.tri(psiMean)] <- t(psiMean)[upper.tri(psiMean)]
 
-# preds_b <- as.data.frame(data$Z_s %*% coef_b)
-# bayes_rocs <- validate_roc(preds_b, data$labels, data$train, data$test, data$Z_s, data$Y_s)
-# lines(bayes_rocs$roc, col = 2) # You can't tell the difference
-
+gPsi <- ggplot(reshape2::melt(solve(psiMean))) +
+    geom_tile(aes(Var1, Var2, fill = value)) +
+    labs(x = "row", y = "col")
+ggsave("./images/psiMat.png", gPsi, "png",
+       width = 6, height = 6, units = "in", dpi = 150)
