e9e2c0673a214e65dbda339a3500ab9a3a241c85
ybaek <overhead012@gmail.com>
2021-02-23 18:17:29 +0900

    EDA script

---
+++ eda.r
@@ -0,0 +1,72 @@
+# Smoothing the quantiles (problem: measurements are integers)
+# Simple moving average
+center <- readRDS("data/stats.rds")
+ds <- readRDS("data/dataset.rds")
+series <- center[[1]]["q5", ]
+ma <- numeric(0)
+for (i in seq(length(series))) {
+    subseries <- window(series, start = max(i - 7, 1), end = min(i + 7, 768))
+    ma <- c(ma, mean(subseries))
+}
+
+library(ggplot2)
+library(reshape2)
+plot(ds$Z[1,], ylim = c(0, max(ds$Z)), type = "l")
+lines(ds$Z[1,] - ma, col = 2)
+example_m <- matrix(ds$Y[1,], 8)
+center_m  <- matrix(center[[2]]["q5", ], 8)
+ggplot(melt(example_m)) + geom_tile(aes(x = Var1, y = Var2, fill = value)) + 
+    scale_fill_distiller(palette = "Reds", lim = c(0, max(ds$Y, na.rm = T)), direction=1)
+ggplot(melt(example_m - center_m)) + geom_tile(aes(x = Var1, y = Var2, fill = value)) +
+    scale_fill_distiller(palette = "Reds", lim = c(0, max(ds$Y, na.rm = T)), direction=1)
+
+# I don't know if this analysis is worthwhile or not, but
+# let's try a k-median per each patient's image
+library(cluster)
+Y_centered <- sweep(ds$Y, 2, c(center_m))
+# Out-of-shelf clustering algorithms aren't good at handling missing values
+Y_centered[is.na(Y_centered)] <- mean(Y_centered, na.rm = T)
+clusters <- apply(Y_centered, 1, function(row) cluster::pam(row, k = 16)$id.med)
+clusters <- matrix(table(clusters), 8)
+g <- ggplot(melt(clusters)) + geom_tile(aes(x = Var1, y = Var2, fill = value)) + 
+    scale_fill_distiller(palette = "Reds", direction=1) + 
+    labs(fill = "Frequency", title = "In how many images was it a medoid (k = 16)?") + 
+    theme_minimal()
+ggsave("images/medoids.png", g, device = "png")
+
+#####
+# How does nonlinearity (tanh) affect the emission model?
+rm(list=ls())
+gc(reset=T)
+N <- 1000
+L <- chol(matrix(c(1, .9, .9, 1), 2))
+x <- matrix(rnorm(2000), 1000) 
+modes <- rbind(c(-2, 2), c(2, -2), c(1, 0), c(0, 1))
+groups <- sample(4, 1000, replace = T)
+y <- x + modes[groups, ]
+png("images/original.png", 1080, 1080, res = 200)
+plot(y, xlab = "", ylab = "", main = "Mixture of 4 BVNS")
+points(modes, col = 2, cex = 2, pch = 19)
+dev.off()
+
+png("images/no_jitter.png", 1080, 1080, res = 200)
+par(mfrow = c(2, 2),
+    mar = c(3, 3, 2, 1), mgp = c(2, .4, 0))
+plot(tanh(y), xlab = "", ylab = "", main = "alpha = 1")
+plot(tanh(y/.1), xlab = "", ylab = "", main = "alpha = .1")
+plot(tanh(y/2), xlab = "", ylab = "", main = "alpha = 2")
+plot(tanh(y/10), xlab = "", ylab = "", main = "alpha = 10")
+dev.off()
+
+png("images/jitter.png", 1080, 1080, res = 200)
+par(mfrow = c(2, 2),
+    mar = c(3, 3, 2, 1), mgp = c(2, .4, 0))
+plot(tanh(y) + .1 * matrix(rnorm(2000), ncol = 2) %*% L,
+    xlab = "", ylab = "", main = "alpha = 1")
+plot(tanh(y/.1) + .1 * matrix(rnorm(2000), ncol = 2) %*% L,
+    xlab = "", ylab = "", main = "alpha = .1")
+plot(tanh(y/2) + .1 * matrix(rnorm(2000), ncol = 2) %*% L,
+    xlab = "", ylab = "", main = "alpha = 2")
+plot(tanh(y/10) + .1 * matrix(rnorm(2000), ncol = 2) %*% L,
+    xlab = "", ylab = "", main = "alpha = 10")
+dev.off()
