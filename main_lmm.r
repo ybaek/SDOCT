f6bbb216d8e8f2207d2fa7825b21307ae4276304
ybaek <overhead012@gmail.com>
2021-07-26 21:56:11 -0400

    Tidied up code

--- main_lmm.r
+++ main_lmm.r
@@ -2,18 +2,20 @@
 utils <- new.env()
 source("utilities.r", local = utils)
 source("main_processing.r")
+source("my_gibbs.r")
 mcmc_opts <- list(N = 10000, delta = 1)
-prior_Prec <- utils$form_kernel_matrix(distMat, .25)[knots, knots]
-kernel_choices <- c(.5, 1, 2, 8)
+prior_Prec <- solve(exp(-distMat^2 * .25)[knots, knots])
+kernel_choices <- c(1, 2, 4, 6)
 results <- vector(mode = "list", length = 4)
 for (iter in 1:4) {
     Kmat <- utils$form_kernel_matrix(z_train, kernel_choices[iter])
     K_svd <- svd(Kmat)
     cumprop <- cumsum(K_svd$d^2) / sum(K_svd$d^2)
-    cutoff  <- min(which(cumprop > .9))
+    cutoff  <- min(which(cumprop > .99))
     U_svd <- K_svd$u[, 1:cutoff]
     d_svd <- K_svd$d[1:cutoff]
-    mcmc_opts$delta <- kernel_choices[iter] * .8 # only heuristic
+    mcmc_opts$delta <- .1
     results[[iter]] <-
         my_gibbs(y_train, U_svd, d_svd, prior_Prec, distMat, knots, mcmc_opts)
 }
+saveRDS(results, "fit_results.rds")
--- my_gibbs.r
+++ my_gibbs.r
@@ -10,19 +10,20 @@ my_gibbs <- function(Y, U_svd, d_svd, prior_Prec, distMat, knots, mcmc_opts) {
     mis_inds <- which(is.na(Y), arr.ind = T)
     Nmis <- dim(mis_inds)[1]
     #
-    npars <- as.integer(
-        # Total no. of things sampled using MCMC
-        (D * Qstar) + (Qstar * (Qstar + 1) / 2) + Nmis + 1 + 1
-    )
-    pars <- matrix(N_samples, npars) # Parameter space
-    lps  <- matrix(N_samples, N) # Observable units space
-    acceptances <- numeric(N_samples) # For Metropolis alg.
-    #
     theta0 <- colMeans(Y, na.rm = T)
     theta <- matrix(rnorm(D * Qstar), D)
-    theta_covinv <- rWishart(1, Qstar + 1, solve(prior_Prec))
+    theta_covinv <- rWishart(1, Qstar + 1, solve(prior_Prec))[, , 1]
     sig2inv <- sd(Y, na.rm = T)^-2
     gamma <- rgamma(1, 3, 3)
+    #
+    npars <- as.integer(
+        # Total no. of things sampled using MCMC
+        length(theta0) + length(theta) + (Qstar * (Qstar + 1) / 2) +
+            1 + 1 + Nmis
+    )
+    pars <- matrix(0, N_samples, npars) # Parameter space
+    lps  <- matrix(0, N_samples, N) # Observable units space
+    acceptances <- numeric(N_samples) # For Metropolis alg.
     # Initialize missing values on the fly,
     # using a spatial neighborhood average
     for (i in seq(Nmis)) {
@@ -37,22 +38,24 @@ my_gibbs <- function(Y, U_svd, d_svd, prior_Prec, distMat, knots, mcmc_opts) {
         )
     }
     #
+    t1 <- Sys.time()
     for (n in seq(N_samples)) {
         convMat <- exp(-distMat[, knots]^2 / gamma)
         theta0_prec <- (1e-4 + sig2inv * N)
         Ymean <- colMeans(Y - U_svd %*% theta %*% t(convMat))
-        theta0 <- rnorm(Q, sig2inv * N / theta0_prec * Ymean, theta0_prec^-.5)
+        theta0 <- rnorm(Q, sig2inv * N / theta0_prec * Ymean, 1 / sqrt(theta0_prec))
         #
-        theta_prec <- theta_covinv %x% diag(d_svd^-2) +
+        theta_prec <- theta_covinv %x% diag(1 / d_svd^2) +
             sig2inv * (crossprod(convMat) %x% crossprod(U_svd))
         Ycentered <- sweep(Y, 2, theta0)
+        Ytrans <- sig2inv * (t(U_svd) %*% Ycentered %*% convMat)
         Rtheta <- chol(theta_prec)
-        theta_det <- backsolve(Rtheta, forwardsolve(t(Rtheta), c(Ycentered)))
+        theta_det <- backsolve(Rtheta, forwardsolve(t(Rtheta), c(Ytrans)))
         theta_random <- backsolve(Rtheta, rnorm(D * Qstar))
         theta <- matrix(theta_det + theta_random, D, Qstar)
         #
-        post_Prec <- prior_Prec + t(theta) %*% diag(d_svd^-2) %*% theta
-        theta_covinv <- rWishart((Qstar + 1) + D / 2, solve(post_Prec))
+        post_Prec <- prior_Prec + t(theta) %*% diag(1 / d_svd^2) %*% theta
+        theta_covinv <- rWishart(1, (Qstar + 1) + D / 2, solve(post_Prec))[, , 1]
         #
         meanMat <- sweep(U_svd %*% theta %*% t(convMat), 2, theta0, "+")
         sse <- sum((Y - meanMat)^2)
@@ -61,16 +64,16 @@ my_gibbs <- function(Y, U_svd, d_svd, prior_Prec, distMat, knots, mcmc_opts) {
         for (i in seq(Nmis)) {
             r <- mis_inds[i, 1]
             j <- mis_inds[i, 2]
-            Y[r, j] <- rnorm(1, meanMat[r, j], sig2inv^-.5)
+            Y[r, j] <- rnorm(1, meanMat[r, j], 1 / sqrt(sig2inv))
         }
         #
         gamma_log_prop <- rnorm(1, log(gamma), delta)
         gamma_prop <- exp(gamma_log_prop)
         convMat_prop <- exp(-distMat[, knots]^2 / gamma_prop)
         meanMat_prop <- sweep(U_svd %*% theta %*% t(convMat_prop), 2, theta0, "+")
-        ll_sum <- sum(dnorm(Y, meanMat, sig2inv^-.5, log = T)) +
+        ll_sum <- sum(dnorm(Y, meanMat, 1 / sqrt(sig2inv), log = T)) +
             dgamma(gamma, 3, 3, log = T)
-        ll_prop_sum <- sum(dnorm(Y, meanMat_prop, sig2inv^-.5, log = T)) +
+        ll_prop_sum <- sum(dnorm(Y, meanMat_prop, 1 / sqrt(sig2inv), log = T)) +
             dgamma(gamma_prop, 3, 3, log = T)
         log_r <- min(0, ll_prop_sum - ll_sum)
         log_u <- log(runif(1))
@@ -86,7 +89,20 @@ my_gibbs <- function(Y, U_svd, d_svd, prior_Prec, distMat, knots, mcmc_opts) {
         )
         lps[n, ] <- rowSums(dnorm(Y, meanMat, sig2inv^-.5, log = T))
         acceptances[n] <- accept
+        if (!(n %% 50)) {
+            # Batch size 50 adaptation suggested by Roberts and Rosenthal
+            ar <- mean(acceptances[1:n])
+            less_opt <- (ar < .44) # the "magic ratio" in 1D -- see Gelman et al.
+            incr <- min(.01, sqrt(50 / n))
+            delta <- delta * exp((-1)^less_opt * incr)
+        }
     }
+    t2 <- Sys.time()
+    #
+    cat(
+        sprintf("Drawing %d samples done in %.2f secs. Metropolis acceptance ratio was %.2f.\n",
+            N_samples, t2 - t1, mean(acceptances))
+    )
     list(
         pars = pars,
         lps  = lps,
