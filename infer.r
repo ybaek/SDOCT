445e627bc62359a65d7b6ac38067b119536b5e91
ybaek <overhead012@gmail.com>
2021-07-27 14:42:47 -0400

    More edits

--- infer.r
+++ infer.r
@@ -26,7 +26,7 @@ K_test  <- utils$form_kernel_matrix(z_test, 2)
 u_test  <- svd(K_test)$u[, 1:4]
 f_hat_test  <- u_test %*% theta_pm
 f_test_full <- sweep(f_hat_test %*% t(convMat), 2, intercept_pm, "+")
-# Comparison: Using raw data necessarily requires imputation
+# (Using raw data necessarily requires imputation -- make a copy)
 mis_inds <- which(is.na(y_test), arr.ind = TRUE)
 y_test2  <- y_test
 y_test2[mis_inds] <- rnorm(nrow(mis_inds), mean(y_test, na.rm = T), sd(y_test, na.rm = T))
@@ -37,60 +37,48 @@ labels_test  <- labels[jj_test]
 ## Second stage "model" wants to actually predict glaucoma labels
 ## To prevent data leakage, training data should be "used twice"
 # Case 1: using raw data summary statistics / training logistic model
-thres <- apply(y_test, 2, median, na.rm = T)
-raw_sdm <- apply(y_test, 1, function(x) mean(x < thres, na.rm = T))
+# raw_sdm <- apply(y_test, 1, function(x) mean(x < .02, na.rm = T))
 rawdes <- cbind(labels_train, demo_df[jj_train, ], y_train)
 rawdespred <- cbind(labels_test, demo_df[jj_test, ], y_test2)
 rawglm <- glm(labels_train ~ . - race_primary, data = rawdes, family = "binomial")
 raw_preds <- predict(rawglm, rawdespred, type = "response")
 # Case 2: using denoised deviations' summary statistics / training logistic model
-model_sdm <- apply(f_test_full, 1, function(x) mean(x < 0.))
+# model_sdm <- apply(f_test_full, 1, function(x) mean(x < 0.))
 des <- cbind(labels_train, demo_df[jj_train, ], f_hat_full)
 despred <- cbind(demo_df[jj_test, ], f_test_full)
 glmfit <- glm(labels_train ~ . - race_primary, data = des, family = "binomial")
 model_preds <- predict(glmfit, despred, type = "response")
 #
-roc_preds1 <- pROC::roc(labels_test, c(model_preds))
-preds_ci1 <- ci.se(roc_preds1, specifities = seq(0, 1, .01))
-roc_preds2 <- pROC::roc(labels_test, c(raw_preds))
-preds_ci2 <- ci.se(roc_preds2, specifities = seq(0, 1, .01))
-roc_sdm1 <- pROC::roc(labels_test, c(model_sdm))
-sdm_ci1 <- ci.se(roc_sdm1, specifities = seq(0, .1, .01))
-roc_sdm2 <- pROC::roc(labels_test, c(raw_sdm))
-sdm_ci2 <- ci.se(roc_sdm2, specifities = seq(0, .1, .01))
+roc_model <- pROC::roc(labels_test, c(model_preds))
+ci_model  <- ci.se(roc_preds1, specifities = seq(0, 1, .01))
+roc_raw <- pROC::roc(labels_test, c(raw_preds))
+ci_raw  <- ci.se(roc_preds2, specifities = seq(0, 1, .01))
+# roc_sdm1 <- pROC::roc(labels_test, c(model_sdm))
+# sdm_ci1 <- ci.se(roc_sdm1, specifities = seq(0, .1, .01))
+# roc_sdm2 <- pROC::roc(labels_test, c(raw_sdm))
+# sdm_ci2 <- ci.se(roc_sdm2, specifities = seq(0, .1, .01))
 #
+unadj_pauc <- function(roc) {
+    # Unnormalized, un-corrected partial AUC in 15% FPR range
+    pROC::auc(roc, partial.auc = c(.85, 1),
+        partial.auto.correct = F, partial.auc.focus = "sensitivity")
+}
 auc_tab <- rbind(
-    c(ci.auc(auc(roc_preds1), method = "boot"),
-      ci.auc(auc(roc_preds2), method = "boot")),
-    c(ci.auc(auc(roc_sdm1), method = "boot"),
-      ci.auc(auc(roc_sdm2), method = "boot"))
+    c(ci.auc(auc(roc_model), method = "boot"),
+      ci.auc(auc(roc_raw), method = "boot"))
 )
 pauc_tab <- rbind(
-    c(ci.auc(auc(roc_preds1, partial.auc = c(.85, 1), partial.auc.correct = FALSE, partial.auc.focus = "sp")) / .15,
-      ci.auc(auc(roc_preds2, partial.auc = c(.85, 1), partial.auc.correct = FALSE, partial.auc.focus = "sp")) / .15),
-    c(ci.auc(auc(roc_sdm1, partial.auc = c(.85, 1), partial.auc.correct = FALSE, partial.auc.focus = "sp")) / .15,
-      ci.auc(auc(roc_sdm2, partial.auc = c(.85, 1), partial.auc.correct = FALSE, partial.auc.focus = "sp")) / .15)
+    c(ci.auc(unadj_pauc(roc_model)) / (1 - .85),
+      ci.auc(unadj_pauc(roc_raw)) / (1 - .85))
 )
+roc.test(auc(roc_model), auc(roc_raw)) # p == .028
+roc.test(unadj_pauc(roc_model), unadj_pauc(roc_raw))
 #
 png("images/roc_logistic.png")
-plot(roc_preds1, col = 4)
-plot(roc_preds2, add = T, col = 2)
-plot(preds_ci1, type = "shape", col = scales::alpha(4, .2), no.roc = TRUE)
-plot(preds_ci2, type = "shape", col = scales::alpha(2, .2), no.roc = TRUE)
-abline(v = .85, lty = 2)
-legend(
-    "bottomright",
-    legend = c("Raw", "Model"),
-    col    = c(2, 4),
-    lty    = 1
-)
-dev.off()
-#
-png("images/roc_sdm.png")
-plot(roc_sdm1, col = 4)
-plot(roc_sdm2, add = T, col = 2)
-plot(sdm_ci1, type = "shape", col = scales::alpha(4, .2), no.roc = TRUE)
-plot(sdm_ci2, type = "shape", col = scales::alpha(2, .2), no.roc = TRUE)
+plot(roc_model, col = 4)
+plot(roc_raw, add = T, col = 2)
+plot(ci_model, type = "shape", col = scales::alpha(4, .2), no.roc = TRUE)
+plot(ci_raw, type = "shape", col = scales::alpha(2, .2), no.roc = TRUE)
 abline(v = .85, lty = 2)
 legend(
     "bottomright",
